5af5a94f57e885ab22f3a7edc8afe9639ae4aa88
Eugene Bespalov <104727292+KAHADOC@users.noreply.github.com>
2023-10-16 14:44:30 +0300

    ohh

---
+++ lesson7_hw/Book.java
@@ -0,0 +1,54 @@
+// com.company.project.lesson07.Book
+package edu.java.itmo_course.hw07;
+
+import java.util.Arrays;
+
+public class Book {
+    // модификатор private - свойство, конструктор, метод доступны только в текущем классе
+    private String name; // значение по умолчанию null (как и для всех ссылочных типов)
+    private boolean isPublished; // // значение по умолчанию false (для типа boolean)
+    // хранит ссылки на нескольких авторов
+    private Author[] authors;
+
+    // ПКМ -> Generate -> Constructor
+    // numberOfAuthors - размер массива authors
+    // значение numberOfAuthors должно находиться в диапазоне [1, 5)
+    public Book(String name, int numberOfAuthors) throws Exception {
+
+        Exception oops = new Exception("oops, too many dudes!");
+        if (numberOfAuthors < 1 || numberOfAuthors >= 5) throw oops;
+        setName(name); // вызов метода внутри класса
+        authors = new Author[numberOfAuthors];
+    }
+
+    // ПКМ -> Generate -> Setter
+    public void setName(String name) { // Setter
+        // if (name == null) return;
+        if (name == null) { // если значение name равно null
+            // будет выброшено исключение, программа завершит выполнение с ошибкой
+            throw new IllegalArgumentException("name not null");
+        }
+        this.name = name;
+    }
+
+    // ПКМ -> Generate -> Getter
+    public String getName() { // Getter
+        return name;
+    }
+    // ПКМ -> Generate -> Setter and Getter
+
+    // метод добавления нового автора в массив authors
+    public void addAddAuthor(Author author) {
+        // author не может быть null ссылкой
+        // новые авторы не должны перезаписывать уже существующих в массиве
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        // Task 1
+        Book book01 = new Book("Тереза Батиста, уставшая воевать", 3);
+        System.out.println(Arrays.toString(book01.authors));
+        book01.authors[0]= new Author(1,"Жоржи Амаду");
+        System.out.println(book01.authors[0].getInfo());
+    }
+}
---
+++ lesson7_hw/Cat.java
@@ -0,0 +1,4 @@
+package edu.java.itmo_course.hw07;
+
+public class Cat {
+}
